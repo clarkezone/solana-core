e3de06b2b65b7d590c011231686e316e2f631954
James Clarke <james@clarkezone.net>
2022-11-26 18:36:41 -0800

    Update readme

--- solana-intro-client/README.md
+++ solana-intro-client/README.md
@@ -1,1 +1,11 @@
 # solana-npx-client-template
+
+```bash
+npm install
+npm start
+```
+
+First run will not correctly airdrop
+in order for airdorp to succeed, switch to a different wireless network
+To airdrop using CLI do: `solana airdrop 2 BDwvrEkAdYmfvNx8kcHK7P36FxmsR4zCxWoMdQn1Gi7p --url devnet`
+
--- solana-intro-client/src/index.ts
+++ solana-intro-client/src/index.ts
@@ -1,4 +1,3 @@
-// We're adding these
 import * as Web3 from '@solana/web3.js';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
@@ -7,27 +6,27 @@ const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9
 const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
 
 async function initializeKeypair(
-    connection: Web3.Connection
-  ): Promise<Web3.Keypair> {
-    if (!process.env.PRIVATE_KEY) {
-      console.log('Generating new keypair... 🗝️');
-      const signer = Web3.Keypair.generate();
-  
-      console.log('Creating .env file');
-      fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
-  
-      return signer;
-    }
-  
-    const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
-    const secretKey = Uint8Array.from(secret);
-    const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+  connection: Web3.Connection
+): Promise<Web3.Keypair> {
+  if (!process.env.PRIVATE_KEY) {
+    console.log('Generating new keypair... 🗝️');
+    const signer = Web3.Keypair.generate();
+
+    console.log('Creating .env file');
+    fs.writeFileSync('.env', `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+
+    return signer;
+  }
+
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? '') as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
 
   // When creating it from the secret key
   await airdropSolIfNeeded(keypairFromSecret, connection);
 
-    return keypairFromSecret;
-  }
+  return keypairFromSecret;
+}
 
 async function airdropSolIfNeeded(
   signer: Web3.Keypair,
@@ -59,50 +58,50 @@ async function airdropSolIfNeeded(
 }
 
 async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
-    const transaction = new Web3.Transaction()
-    const instruction = new Web3.TransactionInstruction({
-      // Instructions need 3 things 
-      
-      // 1. The public keys of all the accounts the instruction will read/write
-      keys: [
-        {
-          pubkey: PROGRAM_DATA_PUBLIC_KEY,
-          isSigner: false,
-          isWritable: true
-        }
-      ],
-      
-      // 2. The ID of the program this instruction will be sent to
-      programId: PROGRAM_ID
-      
-      // 3. Data - in this case, there's none!
-    })
-  
-    transaction.add(instruction)
-    const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
-  
-    console.log(
-      `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
-      )
-  }
-  
+  const transaction = new Web3.Transaction()
+  const instruction = new Web3.TransactionInstruction({
+    // Instructions need 3 things 
+
+    // 1. The public keys of all the accounts the instruction will read/write
+    keys: [
+      {
+        pubkey: PROGRAM_DATA_PUBLIC_KEY,
+        isSigner: false,
+        isWritable: true
+      }
+    ],
+
+    // 2. The ID of the program this instruction will be sent to
+    programId: PROGRAM_ID
+
+    // 3. Data - in this case, there's none!
+  })
+
+  transaction.add(instruction)
+  const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+
+  console.log(
+    `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  )
+}
+
 
 async function main() {
-    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
-    const signer = await initializeKeypair(connection);
-  
-    console.log("Public key:", signer.publicKey.toBase58());
-    await pingProgram(connection, signer)
+  const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
+  const signer = await initializeKeypair(connection);
+
+  console.log("Public key:", signer.publicKey.toBase58());
+  await pingProgram(connection, signer)
 }
 
 
 
 main()
-    .then(() => {
-        console.log("Finished successfully")
-        process.exit(0)
-    })
-    .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+  .then(() => {
+    console.log("Finished successfully")
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
